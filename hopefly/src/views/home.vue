dcac6a4ba21b7302cd0920af83e6a22424aa1c15
招雄彬 <614619114@qq.com>
2020-11-02 21:15:59 +0800

    home 页面OK

--- hopefly/src/views/home.vue
+++ hopefly/src/views/home.vue
@@ -1,19 +1,12 @@
 <template>
-  <Topnav />
   <div>
-    <div class="topNav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>导航1</li>
-        <li>导航2</li>
-      </ul>
-    </div>
+    <Topnav />
     <div class="banner">
       <h1>UI Name</h1>
       <h2>one good UI frame</h2>
       <p class="actions">
-        <a href="">GitHub</a>
-        <a href="">开始</a>
+        <a class="btn" href="https://github.com/lookguy">GitHub</a>
+        <router-link class="btn" to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -26,3 +19,26 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.banner {
+  width: 40%;
+  box-shadow: 4px 7px 7px 3px #eee;
+  margin: 1rem auto;
+  text-align: center;
+  padding: 2rem 1rem;
+  .actions {
+    width: 100%;
+    .btn {
+      min-width: 5em;
+      display: inline-block;
+      padding: 0.2em 0.75em;
+      font-size: 1.4rem;
+      box-shadow: 0 0 10px 2px rgb(219, 73, 54);
+      margin-right: 2em;
+      color: grey;
+      text-decoration: none;
+      border-radius: 0 2rem;
+    }
+  }
+}
+</style>
