8b96dfce1e293af1d531085e9d17f8942b0465fe
招雄彬 <614619114@qq.com>
2020-11-02 21:16:25 +0800

    初始化doc

--- hopefly/src/views/Doc.vue
+++ hopefly/src/views/Doc.vue
@@ -2,7 +2,15 @@
   <div>
     <Topnav />
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>组件 Switch</li>
+          <li>组件 Button</li>
+          <li>组件 Dialog</li>
+          <li>组件 Tabs</li>
+        </ol>
+      </aside>
       <main>主内容</main>
     </div>
   </div>
