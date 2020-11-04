b78fc7aaa16730e58671f8ea9834f40f89001d57
招雄彬 <614619114@qq.com>
2020-11-04 21:01:05 +0800

    点击可切换

--- hopefly/src/lib/Switch.vue
+++ hopefly/src/lib/Switch.vue
@@ -1,6 +1,18 @@
 <template>
-  <button><span></span></button>
+  <button :class="{ checked }" @click="toggle"><span></span></button>
 </template>
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return { checked, toggle };
+  },
+};
+</script>
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
@@ -18,10 +30,11 @@ span {
   left: 2px;
   height: $h2;
   width: $h2;
-  background: green;
+  background: white;
   border-radius: $h2 / 2;
 }
-button:hover > span {
+button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+  background: yellowgreen;
 }
 </style>
