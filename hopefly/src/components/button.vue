24711c3ff91716edd932164036f9194f66d014ed
招雄彬 <614619114@qq.com>
2020-11-15 23:20:46 +0800

    先完成一些基本的样式分离和基本思路

--- hopefly/src/components/button.vue
+++ hopefly/src/components/button.vue
@@ -3,12 +3,28 @@
   <h1>示例1</h1>
   <div>
     <Button>确认</Button>
+    <Button theme="button">确认</Button>
+    <Button theme="link">确认</Button>
+    <Button theme="text">确认</Button>
+  </div>
+  <h1>示例2</h1>
+  <div>
+    <Button>确认</Button>
+    <Button size="normal">确认</Button>
+    <Button size="large">确认</Button>
+    <Button size="small">确认</Button>
   </div>
 </template>
 
 <script lang="ts">
 import Button from "../lib/Button.vue";
 export default {
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
   components: { Button },
 };
 </script>
--- hopefly/src/lib/Button.vue
+++ hopefly/src/lib/Button.vue
@@ -1,9 +1,36 @@
 <template>
-  <button>
+  <button v-bind="rest" class="hopefly-button" :class="classes">
     <slot />
   </button>
 </template>
 
 <script lang="ts">
-export default {};
+import { computed } from "vue";
+export default {
+  inheritAttrs: false,
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+    size: {
+      type: String,
+      default: "normal",
+    },
+  },
+  setup(props, context) {
+    const { theme, size } = props;
+    const classes = computed(() => {
+      return {
+        [`hopefly-theme-${theme}`]: theme,
+        [`hopefly-theme-${size}`]: size,
+      };
+    });
+    const { ...rest } = context.attrs;
+    return { rest, classes };
+  },
+};
 </script>
+
+<style lang="scss">
+</style>
